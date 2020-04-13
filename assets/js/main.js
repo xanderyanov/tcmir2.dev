6d0a0a74e9503bdd6d7f206e362d320dde051f8b
xanderyanov <xanderyanov@gmail.com>
2020-04-13 18:21:11 +0400

    13-04-2020-18-20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,47 @@
-$(function () {});
+$(function () {
+  if ($(".swiper-container1").length) {
+    var mySwiper1 = new Swiper(".swiper-container1", {
+      slidesPerView: 1,
+      loop: true,
+      autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination1",
+        type: "bullets",
+        dynamicBullets: false,
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+      },
+      on: {
+        init: function () {
+          console.log("swiper initialized");
+          $(".swiper-slide").children(".swiper__cadr").removeClass("bounceInRight").fadeOut(500);
+
+          setTimeout(function () {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("bounceInRight");
+          }, 500);
+        },
+        slideChange: function () {
+          $(".swiper-slide").children(".swiper__cadr").removeClass("bounceInRight").fadeOut(500);
+
+          setTimeout(function () {
+            $(".swiper-slide-active")
+              .children(".swiper__cadr")
+              .fadeIn(500)
+              .addClass("animated")
+              .addClass("bounceInRight");
+          }, 500);
+        },
+      },
+    });
+  }
+});
